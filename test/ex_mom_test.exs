8e5aa1ba30e3f8202b17797612b2a52bc44b89ae
Adilson Lima <adilson.pereira.lima@gmail.com>
2022-07-06 19:37:51 -0300

    finished tests

--- test/ex_mom_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ExMomTest do
-  use ExUnit.Case
-  doctest ExMom
-
-  test "greets the world" do
-    assert ExMom.hello() == :world
-  end
-end
--- test/game_test.exs
+++ test/game_test.exs
@@ -1,12 +1,185 @@
 defmodule ExMon.GameTest do
-  use ExUnite.case()
+  use ExUnit.Case
 
-  alias ExMon.Player
+  alias ExMon.{Game, Player}
 
   describe "start/2" do
     test "starts the game state" do
       player = Player.build("Adilson", :chute, :soco, :cura)
       computer = Player.build("Robotinik", :chute, :soco, :cura)
+
+      assert {:ok, _pid} = Game.start(computer, player)
+    end
+  end
+
+  describe "info/0" do
+    test "returns the current game state" do
+      player = Player.build("Adilson", :chute, :soco, :cura)
+      computer = Player.build("Robotinik", :chute, :soco, :cura)
+
+      Game.start(computer, player)
+
+      expected_response = %{
+        computer: %ExMon.Player{
+          life: 100,
+          moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute},
+          name: "Robotinik"
+        },
+        player: %ExMon.Player{
+          life: 100,
+          moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute},
+          name: "Adilson"
+        },
+        status: :started,
+        turn: :player
+      }
+
+      assert expected_response == Game.info()
+    end
+  end
+
+  describe "update/1" do
+    test "returns the game state updated" do
+      player = Player.build("Adilson", :chute, :soco, :cura)
+      computer = Player.build("Robotinik", :chute, :soco, :cura)
+
+      Game.start(computer, player)
+
+      expected_response = %{
+        computer: %ExMon.Player{
+          life: 100,
+          moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute},
+          name: "Robotinik"
+        },
+        player: %ExMon.Player{
+          life: 100,
+          moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute},
+          name: "Adilson"
+        },
+        status: :started,
+        turn: :player
+      }
+
+      assert expected_response == Game.info()
+
+      new_state = %{
+        computer: %ExMon.Player{
+          life: 85,
+          moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute},
+          name: "Robotinik"
+        },
+        player: %ExMon.Player{
+          life: 50,
+          moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute},
+          name: "Adilson"
+        },
+        status: :started,
+        turn: :player
+      }
+
+      Game.update(new_state)
+
+      expected_response = %{new_state | turn: :computer, status: :continue}
+
+      assert expected_response == Game.info()
+    end
+  end
+
+  describe "player/0" do
+    test "return player when get info()" do
+      player = Player.build("Adilson", :chute, :soco, :cura)
+      computer = Player.build("Robotinik", :chute, :soco, :cura)
+
+      Game.start(computer, player)
+
+      expected_response = %{
+        computer: %ExMon.Player{
+          life: 100,
+          moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute},
+          name: "Robotinik"
+        },
+        player: %ExMon.Player{
+          life: 100,
+          moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute},
+          name: "Adilson"
+        },
+        status: :started,
+        turn: :player
+      }
+
+      assert expected_response == Game.info()
+
+      expected_response = %ExMon.Player{
+        life: 100,
+        moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute},
+        name: "Adilson"
+      }
+
+      assert expected_response == Game.player()
+    end
+  end
+
+  describe "turn/0" do
+    test "return player when get info()" do
+      player = Player.build("Adilson", :chute, :soco, :cura)
+      computer = Player.build("Robotinik", :chute, :soco, :cura)
+
+      Game.start(computer, player)
+
+      expected_response = %{
+        computer: %ExMon.Player{
+          life: 100,
+          moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute},
+          name: "Robotinik"
+        },
+        player: %ExMon.Player{
+          life: 100,
+          moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute},
+          name: "Adilson"
+        },
+        status: :started,
+        turn: :player
+      }
+
+      assert expected_response == Game.info()
+
+      expected_response = :player
+
+      assert expected_response == Game.turn()
+    end
+  end
+
+  describe "fetch_player/1" do
+    test "return fetch_player" do
+      player = Player.build("Adilson", :chute, :soco, :cura)
+      computer = Player.build("Robotinik", :chute, :soco, :cura)
+
+      Game.start(computer, player)
+
+      expected_response = %{
+        computer: %ExMon.Player{
+          life: 100,
+          moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute},
+          name: "Robotinik"
+        },
+        player: %ExMon.Player{
+          life: 100,
+          moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute},
+          name: "Adilson"
+        },
+        status: :started,
+        turn: :player
+      }
+
+      assert expected_response == Game.info()
+
+      expected_response = %ExMon.Player{
+        life: 100,
+        moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute},
+        name: "Robotinik"
+      }
+
+      assert expected_response == Game.fetch_player(:computer)
     end
   end
 end
